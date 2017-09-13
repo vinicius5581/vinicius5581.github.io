8dd74b11c881a2921fd8d115c6e8688a9aa22c86
Vinicius Santana <vini@datacoral.co>
2017-09-12 23:47:12 -0700

    FOrm submission tracking

---
+++ js/emailSubmiter.js
@@ -0,0 +1,51 @@
+var emailSender = function (e) {
+  e.preventDefault();
+  var formId = $(e.target.parentNode.parentNode).attr('id');
+  var emailInput = e.target.parentNode.getElementsByClassName('emailInput');
+  var emailInputValue = e.target.parentNode.getElementsByClassName('emailInput')[0].value;
+  var submitButton = e.target;
+  var errorMsg = e.target.parentNode.parentNode.getElementsByClassName('formErrorMsg');
+  var emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+  if (emailValidation.test(emailInputValue)) {
+
+
+    ga( 'send', 'event', 'Contact Form', 'submit' );
+
+    $(submitButton).removeClass('error');
+    $(errorMsg).removeClass('error');
+    window.datacoral('trackUnstructEvent', {
+      name: 'submitted_form',
+      data: {
+        'email': emailInputValue,
+        'nodeId': formId,
+      }
+    });
+    emailInputValue = '';
+    modal.style.display = "flex";
+    setTimeout(function(){
+      modal.style.display = "none";
+    }, 2000);
+  }	else {
+    $(submitButton).addClass('error');
+    $(errorMsg).addClass('error');
+  }
+  // When the user clicks on <span> (x), close the modal
+  modal.onclick = function() {
+      modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+}
+
+$("#emailFormTop .emailSubmit").click(function(e){
+  emailSender(e);
+});
+$("#emailFormBottom .emailSubmit").click(function(e){
+  emailSender(e);
+});
